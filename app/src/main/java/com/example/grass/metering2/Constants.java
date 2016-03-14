8923b65d93b77def1beabea018e70eeda1f81640
yaroslavromanyuta <yaroslavromanyuta@mail.ru>
2016-03-14 11:20:42 +0200

    text changed

--- app/src/main/java/com/example/grass/metering2/Constants.java
+++ app/src/main/java/com/example/grass/metering2/Constants.java
@@ -9,7 +9,7 @@ public final class Constants {
     public static final String TEXT_SIZE_PREF = "textSize";
     public static final String VAL = "valid";
     public static final String LAST_LISOSIKA = "lastLisosika";
-    public static final String DRIVE_URL = "jK_fNcPk7qAAAAAAAAAACIMlLHQwXPu5MjYRbgZTQawrjJdGMAHPu_0c8s4SvMjl";
+    public static final String DRIVE_URL = "jK_fNcPk7qAAAAAAAAAACUyyj28iOR3tO097qJAM4Qmvdw-Pk3nsH6U39CigG-8S";
     public static final String INTENT_MODE_EDIT = "edit";
     public static final String INTENT_MODE_NEW = "new";
     public static final String INTENT_MODE = "mode";
